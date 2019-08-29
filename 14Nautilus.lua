06e69948cb75bbe3eca9829004c9afef4f2db850
samuelchow1997 <samuelchow1997@gmail.com>
2019-08-30 00:41:04 +0800

    Add files via upload

--- 14Nautilus.lua
+++ 14Nautilus.lua
@@ -1,25 +1,31 @@
 require 'MapPositionGOS'
-
-if (myHero.charName ~= "Nautilus") then 
-    return
-end
-
-
-
-if not FileExist(COMMON_PATH .. "GamsteronPrediction.lua") then
-	print("GsoPred. installed Press 2x F6")
-	--DownloadFileAsync("https://raw.githubusercontent.com/gamsteron/GOS-External/master/Common/GamsteronPrediction.lua", COMMON_PATH .. "GamsteronPrediction.lua", function() end)
-	while not FileExist(COMMON_PATH .. "GamsteronPrediction.lua") do end
-end
-    
 require('GamsteronPrediction')
 
-local  TS, OB, DMG, SPELLS
-local myHero = myHero
-local LocalGameTimer = Game.Timer
-GamCore = _G.GamsteronCore
+local GameHeroCount     = Game.HeroCount
+local GameHero          = Game.Hero
+local TableInsert       = _G.table.insert
+
+local orbwalker         = _G.SDK.Orbwalker
+local TargetSelector    = _G.SDK.TargetSelector
+
+local lastQ = 0
+local lastW = 0
+local lastE = 0
+local lastR = 0
+local lastIG = 0
+local lastMove = 0
 
 local lineQ
+
+local Enemys =   {}
+local Allys  =   {}
+
+local function GetDistanceSquared(vec1, vec2)
+    local dx = vec1.x - vec2.x
+    local dy = (vec1.z or vec1.y) - (vec2.z or vec2.y)
+    return dx * dx + dy * dy
+end
+
 
 local function IsValid(unit)
     if (unit 
@@ -28,25 +34,69 @@ local function IsValid(unit)
         and unit.alive 
         and unit.visible 
         and unit.networkID 
-        and unit.health > 0) then
+        and unit.health > 0
+        and not unit.dead
+    ) then
         return true;
     end
     return false;
 end
 
 local function Ready(spell)
-    return myHero:GetSpellData(spell).currentCd == 0 and myHero:GetSpellData(spell).level > 0 and myHero:GetSpellData(spell).mana <= myHero.mana and Game.CanUseSpell(spell) == 0
+    return myHero:GetSpellData(spell).currentCd == 0 
+    and myHero:GetSpellData(spell).level > 0 
+    and myHero:GetSpellData(spell).mana <= myHero.mana 
+    and Game.CanUseSpell(spell) == 0
+end
+
+local function OnAllyHeroLoad(cb)
+    for i = 1, GameHeroCount() do
+        local obj = GameHero(i)
+        if obj.isAlly then
+            cb(obj)
+        end
+    end
+end
+
+local function OnEnemyHeroLoad(cb)
+    for i = 1, GameHeroCount() do
+        local obj = GameHero(i)
+        if obj.isEnemy then
+            cb(obj)
+        end
+    end
 end
 
 class "Nautilus"
 
 function Nautilus:__init()
-    ORB, TS, OB, DMG, SPELLS = _G.SDK.Orbwalker, _G.SDK.TargetSelector, _G.SDK.ObjectManager, _G.SDK.Damage, _G.SDK.Spells
-    self.LastReset = 0
-    self.QData = {Type = _G.SPELLTYPE_LINE, Delay = 0.25, Radius = 90, Range = 1000, Speed = 2000, Collision = true, MaxCollision = 0, CollisionTypes = {_G.COLLISION_MINION, _G.COLLISION_YASUOWALL}, UseBoundingRadius = true}
+    self.QData = {Type = _G.SPELLTYPE_LINE, Delay = 0.25, Radius = 90, Range = 1000, Speed = 2000, Collision = true, MaxCollision = 0, CollisionTypes = {_G.COLLISION_MINION, _G.COLLISION_YASUOWALL}}
+    
     self:LoadMenu()
+
+    OnAllyHeroLoad(function(hero)
+        TableInsert(Allys, hero);
+    end)
+
+    OnEnemyHeroLoad(function(hero)
+        TableInsert(Enemys, hero);
+    end)
+
     Callback.Add("Tick", function() self:Tick() end)
     Callback.Add("Draw", function() self:Draw() end)
+    orbwalker:OnPostAttackTick(function(...) self:OnPostAttack(...) end)
+
+    orbwalker:OnPreMovement(
+        function(args)
+            if lastMove + 180 > GetTickCount() then
+                args.Process = false
+            else
+                args.Process = true
+                lastMove = GetTickCount()
+            end
+        end 
+    )
+
 end
 
 
@@ -55,186 +105,205 @@ end
 
 
 function Nautilus:LoadMenu()
-    LL = MenuElement({type = MENU, id = "ll", name = "Nautilus"})
+    self.tyMenu = MenuElement({type = MENU, id = "14Nautilus", name = "Nautilus"})
     
     --combo
     
-    LL:MenuElement({type = MENU, id = "Combo", name = "Combo"})
-    LL.Combo:MenuElement({name = "Use spell on:", id = "useon", type = _G.MENU})
-    GamCore:OnEnemyHeroLoad(function(hero) LL.Combo.useon:MenuElement({id = hero.charName, name = hero.charName, value = true}) end)
-    LL.Combo:MenuElement({id = "UseQ", name = "[Q]", value = true})
-    LL.Combo:MenuElement({id = "UseW", name = "[W]", value = true})
-    LL.Combo:MenuElement({id = "UseE", name = "[E]", value = true})
+    self.tyMenu:MenuElement({type = MENU, id = "Combo", name = "Combo"})
+        self.tyMenu.Combo:MenuElement({name = "Use spell on:", id = "useon", type = _G.MENU})
+        OnEnemyHeroLoad(function(hero) self.tyMenu.Combo.useon:MenuElement({id = hero.charName, name = hero.charName, value = true}) end)
+        self.tyMenu.Combo:MenuElement({id = "UseQ", name = "[Q]", value = true})
+        self.tyMenu.Combo:MenuElement({id = "range", name = "Max Cast Q In range", value = 1000, min = 1, max = 1000, step = 1})
+        self.tyMenu.Combo:MenuElement({id = "minRange", name = "Min Cast Q In range", value = 0, min = 0, max = 1000, step = 1})
+
+        self.tyMenu.Combo:MenuElement({id = "UseW", name = "[W] AA reset", value = true})
+        self.tyMenu.Combo:MenuElement({id = "UseE", name = "[E]", value = true})
     
     --Harass
-    LL:MenuElement({type = MENU, id = "Harass", name = "Harass"})
-    LL.Harass:MenuElement({name = "Use spell on:", id = "useon", type = _G.MENU})
-    GamCore:OnEnemyHeroLoad(function(hero) LL.Harass.useon:MenuElement({id = hero.charName, name = hero.charName, value = true}) end)
-    LL.Harass:MenuElement({id = "UseQ", name = "Q", value = true})
+    self.tyMenu:MenuElement({type = MENU, id = "Harass", name = "Harass"})
+        self.tyMenu.Harass:MenuElement({name = "Use spell on:", id = "useon", type = _G.MENU})
+        OnEnemyHeroLoad(function(hero) self.tyMenu.Harass.useon:MenuElement({id = hero.charName, name = hero.charName, value = true}) end)
+        self.tyMenu.Harass:MenuElement({id = "UseQ", name = "Q", value = true})
+        self.tyMenu.Harass:MenuElement({id = "range", name = "Max Cast Q In range", value = 1000, min = 1, max = 1000, step = 1})
 
     --Auto
-    LL:MenuElement({type = MENU, id = "Auto", name = "Auto"})
-    LL.Auto:MenuElement({id = "AutoIG", name = "Auto Ingite KS", value = true})
+    self.tyMenu:MenuElement({type = MENU, id = "Auto", name = "Auto"})
+        self.tyMenu.Auto:MenuElement({id = "AutoIG", name = "Auto Ingite KS", value = true})
+        self.tyMenu.Auto:MenuElement({id = "combo", name = "Only Use In Combo", value = true})
 
 
     --Draw
-    LL:MenuElement({type = MENU, id = "Drawing", name = "Drawing"})
-    LL.Drawing:MenuElement({id = "Q", name = "Draw [Q] Range", value = true})
+    self.tyMenu:MenuElement({type = MENU, id = "Drawing", name = "Drawing"})
+        self.tyMenu.Drawing:MenuElement({id = "Q", name = "Draw [Q] Range", value = true})
 
 
 end
 
 function Nautilus:Draw()
-    if myHero.dead then
-        return
-    end
---[[
-    if lineQ ~= nil then
-        lineQ:__draw(1)
-    end
---]]
+    if myHero.dead then return end
 
-    if LL.Drawing.Q:Value() and Ready(_Q) then
+    -- if lineQ ~= nil then
+    --     lineQ:__draw(1,Draw.Color(80 ,0xFF,0xFF,0xFF) )
+    -- end
+
+    if self.tyMenu.Drawing.Q:Value() and Ready(_Q) then
         Draw.Circle(myHero.pos, 1000,Draw.Color(80 ,0xFF,0xFF,0xFF))
     end
 
 end
 
 
-local NextTick = GetTickCount()
 function Nautilus:Tick()
     if myHero.dead or Game.IsChatOpen() or (ExtLibEvade and ExtLibEvade.Evading == true) then
         return
     end
 
     self:Auto()
-    if NextTick > GetTickCount() then return end
-    ORB:SetMovement(true)
-    if ORB.Modes[0] then --combo
+
+    if orbwalker.Modes[0] then --combo
         self:Combo()
-    elseif ORB.Modes[1] then --harass
+    elseif orbwalker.Modes[1] then --harass
         self:Harass()
     end
 
 end
 
-function Nautilus:Combo()
-    local EnemyHeroes = OB:GetEnemyHeroes(1100, false)
-    local targetList = {}
-
-    for i = 1, #EnemyHeroes do
-        local hero = EnemyHeroes[i]
-        local heroName = hero.charName
-        if LL.Combo.useon[heroName] and LL.Combo.useon[heroName]:Value() then
-            targetList[#targetList + 1] = hero
-        end
-    end
-    local target = TS:GetTarget(targetList)
-    if target == nil then return end
-
-
-    if IsValid(target) then
-        if LL.Combo.UseQ:Value() and Ready(_Q) and myHero.pos:DistanceTo(target.pos) <= 1100 then
-
-            local Pred = GetGamsteronPrediction(target, self.QData, myHero)
-            if Pred.Hitchance >= _G.HITCHANCE_HIGH then
-                lineQ = LineSegment(Pred.CastPosition, Pred.CastPosition:Extended(myHero.pos, myHero.pos:DistanceTo(target.pos)))
-                if MapPosition:intersectsWall(lineQ) then
+function Nautilus:CastQ(target)
+    if Ready(_Q) and lastQ +350 < GetTickCount() and orbwalker:CanMove() then
+        local Pred = GetGamsteronPrediction(target, self.QData, myHero)
+        if Pred.Hitchance >= _G.HITCHANCE_HIGH then
+            lineQ = self:CreateQPoly(Pred.CastPosition)
+            for i, lineSegment in ipairs(lineQ:__getLineSegments()) do
+                if MapPosition:intersectsWall(lineSegment) then
                     return
                 end
-                NextTick = GetTickCount() + 750
-                ORB:SetMovement(false)
-                Control.CastSpell(HK_Q, Pred.CastPosition)
-                print("cast Q")
             end
+            Control.CastSpell(HK_Q, Pred.CastPosition)
+            --print("cast Q")
+            lastQ = GetTickCount()
         end
+    end
+end
 
-        if LL.Combo.UseW:Value() and Ready(_W) and myHero.pos:DistanceTo(target.pos) <= 325 then
-            NextTick = GetTickCount() + 100
+function Nautilus:OnPostAttack()
+    if orbwalker.Modes[0] then
+        if self.tyMenu.Combo.UseW:Value() and Ready(_W) and lastW + 350 < GetTickCount() then
             Control.CastSpell(HK_W)
-            print("cast W")
-
+            lastW = GetTickCount()
+            --print('cast W')
         end
+    end
+end
 
+function Nautilus:Combo()
+    local targetList = {}
+    local target
 
-
-        if LL.Combo.UseE:Value() and Ready(_E) and myHero.pos:DistanceTo(target.pos) <= 325 then
-            NextTick = GetTickCount() + 850
-            Control.CastSpell(HK_E)
-            print("cast E")
+    for i = 1, #Enemys do
+        local enemy = Enemys[i]
+        local heroName = enemy.charName
+        if self.tyMenu.Combo.useon[heroName] and self.tyMenu.Combo.useon[heroName]:Value() then
+            targetList[#targetList + 1] = enemy
         end
+    end
 
+    target = self:GetTarget(targetList, self.QData.Range)
 
+    if target and IsValid(target) and self.tyMenu.Combo.UseQ:Value() 
+    and GetDistanceSquared(myHero.pos, target.pos) <= self.tyMenu.Combo.range:Value()^2 
+    and GetDistanceSquared(myHero.pos, target.pos) > self.tyMenu.Combo.minRange:Value()^2
+    then 
+        self:CastQ(target)
+    end
+
+    target = self:GetTarget(Enemys, 325)
+
+    if target and IsValid(target) and self.tyMenu.Combo.UseE:Value() and Ready(_E) and lastE + 550 < GetTickCount() and orbwalker:CanMove() then
+        Control.CastSpell(HK_E)
+        lastE = GetTickCount()
+        --print("cast E")
+        return
     end
 
 end
 
 function Nautilus:Harass()
-    local EnemyHeroes = OB:GetEnemyHeroes(1100, false)
     local targetList = {}
+    local target
 
-    for i = 1, #EnemyHeroes do
-        local hero = EnemyHeroes[i]
-        local heroName = hero.charName
-        if LL.Harass.useon[heroName] and LL.Harass.useon[heroName]:Value() then
-            targetList[#targetList + 1] = hero
+    for i = 1, #Enemys do
+        local enemy = Enemys[i]
+        local heroName = enemy.charName
+        if self.tyMenu.Harass.useon[heroName] and self.tyMenu.Harass.useon[heroName]:Value() then
+            targetList[#targetList + 1] = enemy
         end
     end
-    local target = TS:GetTarget(targetList)
-    if target == nil then return end
 
-    if IsValid(target) then
+    target = self:GetTarget(targetList, self.QData.Range)
 
-        if LL.Harass.UseQ:Value() and Ready(_Q) and myHero.pos:DistanceTo(target.pos) <= 1100 then
-            local Pred = GetGamsteronPrediction(target, self.QData, myHero)
-            if Pred.Hitchance >= _G.HITCHANCE_HIGH then
-                lineQ = LineSegment(Pred.CastPosition, Pred.CastPosition:Extended(myHero.pos, myHero.pos:DistanceTo(target.pos)))
-                if MapPosition:intersectsWall(lineQ) then
-                    return
-                end
-                NextTick = GetTickCount() + 250
-                ORB:SetMovement(false)
-                Control.CastSpell(HK_Q, Pred.CastPosition)
-                print("cast Q")
-            end
-        end
 
+    if target and IsValid(target) and self.tyMenu.Harass.UseQ:Value() and GetDistanceSquared(myHero.pos, target.pos) <= self.tyMenu.Harass.range:Value()^2 then
+        self:CastQ(target)
     end
 
 end
 
 
 function Nautilus:Auto()
-    if ORB.Modes[0] then --combo
+    if self.tyMenu.Auto.combo:Value() and not orbwalker.Modes[0] then return end
 
-        local IGdamage = 50 + 20 * myHero.levelData.lvl
-        local target = TS:GetTarget(600)
-        if target == nil then return end
-        
-        if LL.Auto.AutoIG:Value() then
+    if myHero:GetSpellData(SUMMONER_1).name ~= "SummonerDot" and myHero:GetSpellData(SUMMONER_2).name ~= "SummonerDot" then return end
+    if lastIG + 250 > GetTickCount() then return end
+    local IGdamage = 50 + 20 * myHero.levelData.lvl
+    for enemyk , enemy in pairs(Enemys) do 
+        if IsValid(enemy) and enemy.pos:DistanceTo(myHero.pos) < 600 then
             if myHero:GetSpellData(SUMMONER_1).name == "SummonerDot" and myHero:GetSpellData(SUMMONER_1).currentCd == 0 then
-                if IGdamage >= target.health then
-                    NextTick = GetTickCount() + 250
-                    Control.CastSpell(HK_SUMMONER_1, target.pos)
-                    print("cast ig")
-
+                if IGdamage >= enemy.health then
+                    Control.CastSpell(HK_SUMMONER_1, enemy.pos)
+                    lastIG = GetTickCount()
+                    --print('cast IG')
+                    return
                 end
             end
             
 
+            
             if myHero:GetSpellData(SUMMONER_2).name == "SummonerDot" and myHero:GetSpellData(SUMMONER_2).currentCd == 0 then
-                if IGdamage >= target.health then
-                    NextTick = GetTickCount() + 250
-                    Control.CastSpell(HK_SUMMONER_2, target.pos)
-                    print("cast ig")
-
+                if IGdamage >= enemy.health then
+                    Control.CastSpell(HK_SUMMONER_2, enemy.pos)
+                    lastIG = GetTickCount()
+                    return
                 end
             end
         end
     end
+
 end
 
+function Nautilus:GetTarget(list, range)
+    local targetList = {}
 
-function OnLoad()
-    _G[myHero.charName]()
+    for i = 1, #list do
+        local hero = list[i]
+        if GetDistanceSquared(hero.pos, myHero.pos) < range * range then
+            targetList[#targetList + 1] = hero
+        end
+    end
+
+    return TargetSelector:GetTarget(targetList)
 end
+
+function Nautilus:CreateQPoly(position)
+    local startPos = myHero.pos
+    local endPos = position
+    local width = 10
+    local c1 = startPos+Vector(Vector(endPos)-startPos):Perpendicular():Normalized()*width
+    local c2 = startPos+Vector(Vector(endPos)-startPos):Perpendicular2():Normalized()*width
+    local c3 = endPos+Vector(Vector(startPos)-endPos):Perpendicular():Normalized()*width
+    local c4 = endPos+Vector(Vector(startPos)-endPos):Perpendicular2():Normalized()*width
+  
+    local poly = Polygon(c1,c2,c3,c4)
+
+    return poly
+end
+Nautilus()
